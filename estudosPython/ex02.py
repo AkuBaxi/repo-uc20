48dfc05aae146187f8a3cab0031d45f4af57842b
Haruu <133592073+AkuBaxi@users.noreply.github.com>
2024-11-03 14:18:14 -0300

    Delete estudosPython directory

--- estudosPython/ex02.py
+++
@@ -1,3 +0,0 @@
-#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
-nome = str(input("Digite seu nome:"))
-print(f"Sejá bem vindo , {nome}!")
--- estudosPython/ex05.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
-
-num = int(input("Digite um número:"))
-ant = num-1
-sucess = num+1
-
-print("O número é:", num)
-print("O número  anterior é: ", ant)
-print("O número  posterior é: ", sucess)
